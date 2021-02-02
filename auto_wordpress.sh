cb5fcca2982f7ae0429c01cc9e01140f0248ae46
b4rt <root.jmn@gmail.com>
2021-02-01 23:52:07 -0500

    first commmit

--- auto_wordpress.sh
+++ auto_wordpress.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 while IFS= read -r line; do
     ./generate_wordpress.sh $line
-done < $1
+done < list_domains.txt
--- generate_wordpress.sh
+++ generate_wordpress.sh
@@ -12,34 +12,34 @@ then
     if [ ! -d $ROOT_HTML$DOMAIN ];
     then
         wget https://wordpress.org/latest.tar.gz
-        echo "${GREEN}Creando el directorio ${NC}$ROOT_HTML$DOMAIN"
+        echo "${GREEN}Add dir  ${NC}$ROOT_HTML$DOMAIN"
         sudo mkdir -v $ROOT_HTML$DOMAIN
     
-        echo "${GREEN}Moviendo wordpress.tar.gz"
+        echo "${GREEN}Move wordpress.tar.gz"
         sudo mv -v latest.tar.gz $ROOT_HTML$DOMAIN
     
-        echo "${GREEN}Descomprimiendo $ROOT_HTML$DOMAIN/latest.tar.gz"
+        echo "${GREEN}Untar dir $ROOT_HTML$DOMAIN/latest.tar.gz"
         sudo tar -zxf $ROOT_HTML$DOMAIN"/latest.tar.gz" -C $ROOT_HTML$DOMAIN --no-same-owner
     
-        echo "${RED}Eliminando innecesarios"    
+        echo "${RED} Remove latest.tar.gz"    
         sudo rm -v $ROOT_HTML$DOMAIN"/latest.tar.gz"
         sudo mv -v $ROOT_HTML$DOMAIN"/wordpress" $ROOT_HTML$DOMAIN"/public_html"
     
-        echo "${GREEN}Cambiando 755 directorios  $ROOT_HTML$DOMAIN/public_html"
+        echo "${GREEN} Set chmod 755 to dirs in  $ROOT_HTML$DOMAIN/public_html"
         sudo find $ROOT_HTML$DOMAIN"/public_html" -type d -exec chmod 755 {} \;
     
-        echo "${GREEN}Cambiando 644 ficheros  $ROOT_HTML$DOMAIN/public_html"
+        echo "${GREEN} Set chmod 644  to files in $ROOT_HTML$DOMAIN/public_html"
         sudo find $ROOT_HTML$DOMAIN"/public_html" -type f -exec chmod 644 {} \;
 
-        echo "${NC}Cambiando owner $USER_WEBSERVER:$GROUP_WEBSERVER  en $ROOT_HTML$DOMAIN/public_html"
+        echo "${NC} Change  owner $USER_WEBSERVER:$GROUP_WEBSERVER  in $ROOT_HTML$DOMAIN/public_html"
         sudo chown -R  $USER_WEBSERVER:$GROUP_WEBSERVER $ROOT_HTML$DOMAIN"/public_html";
         ls -al $ROOT_HTML$DOMAIN"/public_html"
     
     else
-        echo "${RED}El directorio/dominio ya existe: $ROOT_HTML$DOMAIN"
+        echo "${RED} Dir: $ROOT_HTML$DOMAIN already exist"
     fi
 
 else
-   echo  "${RED}Ingrese un dominio"
+   echo  "${RED} Set a domain name "
 fi
 
